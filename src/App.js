d6923b525a7a7880245b7b215d6f9097d9e2ea5b
joyeuse-cloud <irambonajoy@gmail.com>
2022-01-03 18:16:42 +0200

    app.css imported

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import HomePage from "./components/home";
 import Login from "./components/login";
+import './App.css';
 import GroupLogin from "./components/glogin";
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HodDashboard from "./components/home/common/hod_dashboard";
